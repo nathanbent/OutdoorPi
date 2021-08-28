74855e0c61ffd5f64fa62e4be030c591237a9a04
nathanbent <61810169+nathanbent@users.noreply.github.com>
2021-08-27 20:23:00 -0400

    Starting!

--- av/record_video.py
+++ av/record_video.py
@@ -1,3 +1,5 @@
-camera.start_recording('/home/pi/Scripts/OutdoorPi/av/video.h264')
+camera.start_preview()
+camera.start_recording('/home/pi/Desktop/video.h264')
 sleep(5)
 camera.stop_recording()
+camera.stop_preview()
