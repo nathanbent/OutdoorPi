43a11c3505c5c5e0171202e37ecbb369ed7ca763
nathanbent <61810169+nathanbent@users.noreply.github.com>
2021-08-27 20:35:21 -0400

    Starting!

--- av/sound_record.py
+++ av/sound_record.py
@@ -1,37 +1,37 @@
 import pyaudio
 import wave
+import os
 
-form_1 = pyaudio.paInt16 # 16-bit resolution
-chans = 1 # 1 channel
-samp_rate = 44100 # 44.1kHz sampling rate
-chunk = 4096 # 2^12 samples for buffer
-record_secs = 3 # seconds to record
-dev_index = 1 # device index found by p.get_device_info_by_index(ii)
-wav_output_filename = 'test1.wav' # name of .wav file
+#The following code comes from markjay4k as referenced below
+form_1 = pyaudio.paInt16
+chans=1
+samp_rate = 44100
+chunk = 4096
+record_secs = 1     #record time
+dev_index = 2
+wav_output_filename = 'test1.wav'
 
-audio = pyaudio.PyAudio() # create pyaudio instantiation
 
-# create pyaudio stream
-stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
-                    input_device_index = dev_index,input = True, \
-                    frames_per_buffer=chunk)
+audio = pyaudio.PyAudio()
+
+#setup audio input stream
+stream=audio.open(format = form_1,rate=samp_rate,channels=chans, input_device_index = dev_index, input=True, frames_per_buffer=chunk)
 print("recording")
-frames = []
+frames=[]
 
-# loop through stream and append audio chunks to frame array
 for ii in range(0,int((samp_rate/chunk)*record_secs)):
-    data = stream.read(chunk)
+    data=stream.read(chunk,exception_on_overflow = False)
     frames.append(data)
 
 print("finished recording")
 
-# stop the stream, close it, and terminate the pyaudio instantiation
 stream.stop_stream()
 stream.close()
 audio.terminate()
 
-# save the audio frames as .wav file
-wavefile = wave.open(wav_output_filename,'wb')
+#creates wave file with audio read in
+#Code is from the wave file audio tutorial as referenced below
+wavefile=wave.open(wav_output_filename,'wb')
 wavefile.setnchannels(chans)
 wavefile.setsampwidth(audio.get_sample_size(form_1))
 wavefile.setframerate(samp_rate)
