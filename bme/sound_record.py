8c7646068af215278f5e4076bc9ac9ea879abf4f
nathanbent <61810169+nathanbent@users.noreply.github.com>
2021-08-27 20:01:10 -0400

    Starting!

--- bme/sound_record.py
+++ bme/sound_record.py
@@ -6,7 +6,7 @@ chans = 1 # 1 channel
 samp_rate = 44100 # 44.1kHz sampling rate
 chunk = 4096 # 2^12 samples for buffer
 record_secs = 3 # seconds to record
-dev_index = 2 # device index found by p.get_device_info_by_index(ii)
+dev_index = 1 # device index found by p.get_device_info_by_index(ii)
 wav_output_filename = 'test1.wav' # name of .wav file
 
 audio = pyaudio.PyAudio() # create pyaudio instantiation
